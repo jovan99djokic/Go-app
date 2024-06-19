c23cd4a971d8ffa91b5144072e69b60a7788e500
jovan99djokic <jovan99djokicpn@gmail.com>
2024-06-19 14:43:01 +0200

    Server is running extra good

--- main.go
+++ main.go
@@ -23,6 +23,6 @@ func main() {
     http.HandleFunc("/hello", hello)
 
     // start the server, listen on localhost port 8000
-    fmt.Println("Jovan's server is now running")
+    fmt.Println("Jovan's server is now running extra good")
     log.Fatal(http.ListenAndServe(":8000", nil))
 }
